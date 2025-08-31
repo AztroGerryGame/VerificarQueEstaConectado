11c2c9d07ef54ebbf596509c59518c4a113800ed
AztroGerryGam <pagargolopez@gmail.com>
2025-08-30 23:30:30 -0600

    commit
    Mandando un saludo a los usuarios desde unity con los log que se usan.

---
+++ primerScript/Assets/Script/holaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Mensaje en desuso");
+        Debug.Log("Hola mundo desde Unity");
+        Debug.LogError("Error a la hora de programar");
+        Debug.LogWarning("Nota de la programacion");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
